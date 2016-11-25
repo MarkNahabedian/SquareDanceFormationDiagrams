28690fcf9f6ce595ec072f71ef506d32cd5d5638
Markk Nahabedian <naha@mit.edu>
2016-11-25 16:32:00 -0500

    Migration of backup-2016-11-25 into git.

--- dancers.js
+++ dancers.js
@@ -23,9 +23,8 @@ var xlink_uri = "http://www.w3.org/1999/xlink";
 
    */
 
-function Floor(svg_id, dancers) {
+function Floor(dancers) {
   // This particular floor
-  this.svg_id = svg_id;
   this.dancers = dancers;
   // Some defsault drawing parameters:
   this.dancer_size = 20;
@@ -36,11 +35,10 @@ function Floor(svg_id, dancers) {
   this.neu_corner_fraction = 0.3;
   // Tell the dancers what floor they're on.
   var floor = this;
-  var i = 1;
-  this.dancers.map(function(d) {
-    d.floor = floor;
-    d.dancer_id = this.svg_id + ":" + i++;
-  });
+  this.dancers.map(function(d) { d.floor = floor; });
+};
+
+Floor.prototype.draw = function(svg_id) {
   // Setting up for drawing:
   var xs = this.dancers.map(function(d) { return d.x; });
   var ys = this.dancers.map(function(d) { return d.y; });
@@ -48,19 +46,19 @@ function Floor(svg_id, dancers) {
   var max_x = Math.max.apply(null, xs);
   var min_y = Math.min.apply(null, ys);
   var max_y = Math.max.apply(null, ys);
-  this.svg_element = document.getElementById(this.svg_id);
-  if (!this.svg_element) {
-    console.log("No svg element with id " + this.svg_id);
+  var svg_element = document.getElementById(svg_id);
+  if (!svg_element) {
+    console.log("No svg element with id " + svg_id);
     return;
   }
   // Add one to provide half a dancer of space at each border.
   var dancers_wide = 1 + max_x - min_x;
   var dancers_high = 1 + max_y - min_y;
   // Does the svg element already have dimensions?
-  console.log("For Floor " + this.svg_id + " the ideal aspect ratio (width/height) is " +
+  console.log("For Floor " + svg_id + " the ideal aspect ratio (width/height) is " +
 	      dancers_wide / dancers_high);
-  var width = this.svg_element.clientWidth;
-  var height = this.svg_element.clientHeight;
+  var width = svg_element.clientWidth;
+  var height = svg_element.clientHeight;
   console.log("svg size: " + width + " x " + height);
   var available = Math.min(width / dancers_wide, height / dancers_high);
   console.log("space available for a dancer: " + available);
@@ -70,10 +68,20 @@ function Floor(svg_id, dancers) {
   	+ (width / 2 - dancers_wide * this.dancer_spacing / 2) + ", "
   	+ (height / 2 - dancers_high * this.dancer_spacing / 2) + ")";
   put_dancers_here.setAttribute("transform", translate);
-  this.svg_element.appendChild(put_dancers_here);
+  svg_element.appendChild(put_dancers_here);
   this.dancers.map(function(d) {
-      put_dancers_here.appendChild(d.svg());
+      put_dancers_here.appendChild(d.svg(svg_id));
   });
+  return this;
+};
+
+Floor.prototype.getDancer = function(label) {
+  if (label instanceof Dancer) return label;
+  for (var i = 0; i < this.dancers.length; i++) {
+    var dancer = this.dancers[i];
+    if (dancer.label == label) return dancer;
+  }
+  return null;
 };
 
 // Makes a Dancer.  x and y are floor positions, typically numbers from 1 to 8.
@@ -85,7 +93,6 @@ function Dancer(x, y, direction, label, gender, color) {
   this.gender = gender || Dancer.gender.NEU;
   this.color = color || "white";
   this.floor = null;
-  this.dancer_id = null;
 };
 
 Dancer.gender = {
@@ -94,9 +101,11 @@ Dancer.gender = {
     NEU: "unspecified"
 };
 
-//Builds an svg:g element for a Dancer and returns it.  It's up to the caller to add it to the DOM tree.
-Dancer.prototype.svg = function() {
+// Builds an svg:g element for a Dancer and returns it.  It's up to the
+// caller to add it to the DOM tree.
+Dancer.prototype.svg = function(floor_svg_id) {
   var g = document.createElementNS(svg_uri, "g");
+  var dancer_id = floor_svg_id + ":" + this.label;
   g.setAttribute("ID", this.dancer_id);
   var rotate = "rotate(" + (180 - this.direction * 90) + ")";
   var translate = "translate("
@@ -119,6 +128,13 @@ Dancer.prototype.svg = function() {
   dancer_shape.setAttribute("stroke", "black");
   g.appendChild(dancer_shape);
   g.appendChild(nose(this));
+
+  var label = document.createElementNS(svg_uri, "text");
+  label.setAttribute("text-anchor", "middle");
+  label.setAttribute("alignment-baseline", "middle");
+  label.appendChild(document.createTextNode(this.label));
+  g.appendChild(label);
+
   return g;
 };
 
@@ -187,25 +203,38 @@ function dancer_neu (dancer) {
     return shape;
 }
 
-/*
-<svg width="640" height="480" xmlns="http://www.w3.org/2000/svg">
- <!-- Created with SVG-edit - http://svg-edit.googlecode.com/ -->
+// Reposition a Dancer by revolving it around some point by an angle.
+// The angle is expressed as a number of walls in promenade direction.
+Dancer.prototype.revolve = function(center_x, center_y, angle) {
+  var delta_x = this.x - center_x;
+  var delta_y = this.y - center_y;
+  var radians = 2 * Math.PI * angle / 4;
+  var sin = Math.sin(radians);
+  var cos = Math.cos(radians);
+  // The dancer coordinate system (X axis ascending to the right, Y
+  // axis ascending down the page) is the opposite handedness from the
+  // cartesian plane, so we change the sign of the change in Y
+  // coordinate.
+  var revolved_x = delta_x * cos + delta_y * sin;
+  var revolved_y = - delta_x * sin + delta_y * cos;
+  this.x = center_x + revolved_x;
+  this.y = center_y + revolved_y;
+  this.direction = (this.direction + angle) % 4;
+};
 
-  <line fill="none" stroke="#000000"
-           stroke-width="1" x1="300" x2="300"  y1="0" y2="480"/>
-  <line fill="none" stroke="#000000"
-           stroke-width="1" x1="0" y1="80" x2="640" y2="80"/>
-  <line fill="none" stroke="#FF0000"
-           stroke-width="1" x1="320" x2="320" y1="0" y2="480"/>
- 
- 
-
-]
- <g id="neu">
-  <path d="m194,227l38,-6l4,13l-17,17l-25,1l-14,-15l14,-10z"
-            stroke-width="1" stroke="#000000" fill="#ffff00"/>
- </g>
-</svg>
-*/
-
+// Rotate the specified dancers around their common center point.
+// Angle is expressed as a number of walls in promenade direction.
+Floor.prototype.rotate = function(angle, dancers) {
+  var floor = this;
+  dancers = dancers.map(function(d) {
+      return floor.getDancer(d);
+  });
+  var plus = function(a, b) { return a + b; };
+  var center_x = dancers.map(function(d) { return d.x; }).reduce(plus)
+    / dancers.length;
+  var center_y = dancers.map(function(d) { return d.y; }).reduce(plus)
+    / dancers.length;
+  dancers.map(function(d) { d.revolve(center_x, center_y, angle); });
+  return this;
+};
 
